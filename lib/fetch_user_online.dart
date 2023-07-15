8b90dfd4003f59ec8c486b76ef7bc26fd8a795ee
johnwillz-ux <johnwillz.ux@gmail.com>
2023-07-15 23:00:45 +0100

    StreamProvide

--- lib/fetch_user_online.dart
+++ lib/fetch_user_online.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:river_pod/user_model.dart';
+
+final userRepoProvider = Provider((ref) => UserRepo());
 
 class UserRepo {
   Future<User> getUserDataOnline() {
